315d4312d1ec534423b59c50c8a0838d0caf6403
NeoJax <NeoJax@gmail.com>
2020-01-02 21:43:07 -0500

    Finished Beautiful days at the movies

---
+++ HackerRankProblems/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(beautifulDays(20, 23, 6));
+        }
+
+        static int beautifulDays(int i, int j, int k)
+        {
+            int count = 0;
+            for (int day = i; day <= j; day++)
+            {
+                int num = day;
+                int reverseNum = 0;
+                while (num > 0)
+                {
+                    int remainder = num % 10;
+                    reverseNum = (reverseNum * 10) + remainder;
+                    num = num / 10;
+                }
+                if ((day - reverseNum) % k == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
