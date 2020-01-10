75c878b2674a3c959805c3f79d7ddbb3217286ef
NeoJax <NeoJax@gmail.com>
2020-01-09 22:32:31 -0500

    Another Problem done

---
+++ Hurdle_Race/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hurdle_Race
+{
+    class Program
+    {
+        static int hurdleRace(int k, int[] height)
+        {
+            int highestPots = 0;
+            for (int i = 0; i < height.Length; i++)
+            {
+                if (k < height[i])
+                {
+                    if ((k+highestPots) < height[i])
+                    {
+                        highestPots = height[i] - k;
+                    }
+                }
+            }
+            return highestPots;
+        }
+    }
+}
